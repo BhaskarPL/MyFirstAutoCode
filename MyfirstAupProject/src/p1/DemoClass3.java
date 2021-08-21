6fc61ecac759752dbc3489b286bb9ba24bd6b9bd
BhaskarPL <bhaskarpl@gmail.com>
2021-08-21 16:50:15 +0530

    ThirdPush toLocal Repo

---
+++ MyfirstAupProject/src/p1/DemoClass3.java
@@ -0,0 +1,13 @@
+package p1;
+
+public class DemoClass3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("My Third trial to push to gitHub");
+		
+	}
+	
+
+}
