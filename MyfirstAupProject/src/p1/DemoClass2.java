cb7d91bc23b1f049fc003ecbcb284c7174b297d2
BhaskarPL <bhaskarpl@gmail.com>
2021-08-21 14:53:54 +0530

    MyCode Update

---
+++ MyfirstAupProject/src/p1/DemoClass2.java
@@ -0,0 +1,12 @@
+package p1;
+
+public class DemoClass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("My Second trial to push to gitHub");
+
+	}
+
+}
