168032543ea0a28aee948996f1ec2bc2c127bb10
BhaskarPL <bhaskarpl@gmail.com>
2021-08-21 14:50:18 +0530

    Myfirst Code in Github repo

---
+++ MyfirstAupProject/src/p1/DemoClass1.java
@@ -0,0 +1,12 @@
+package p1;
+
+public class DemoClass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("My first trial to push to gitHub");
+
+	}
+
+}
