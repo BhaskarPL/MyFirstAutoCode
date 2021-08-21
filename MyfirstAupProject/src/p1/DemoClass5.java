01ddc752b1db663d376a75f6d172494b69899901
BhaskarPL <bhaskarpl@gmail.com>
2021-08-21 16:58:37 +0530

    fifth Push

---
+++ MyfirstAupProject/src/p1/DemoClass5.java
@@ -0,0 +1,11 @@
+package p1;
+
+public class DemoClass5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My fifth  trial to push to gitHub");
+
+	}
+
+}
